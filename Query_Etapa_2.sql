4c7c82521fba1a186816543fa13fc3d1334bf4b9
Marcelo Silveira <silveira.ti@gmail.com>
2021-12-01 23:17:38 -0300

    Adicionado SQL
    Criado

---
+++ Query_Etapa_2.sql
@@ -0,0 +1,13 @@
+﻿SELECT 
+   employees.first_name,
+   employees.last_name,
+   departments.dept_name,
+   DATEDIFF(to_date, from_date) AS Meses
+FROM  dept_emp
+      INNER JOIN departments
+        ON dept_emp.dept_no = departments.dept_no
+      INNER JOIN employees
+        ON dept_emp.emp_no = employees.emp_no
+WHERE year(to_date) < year(CURRENT_DATE()) AND  year(from_date) < year(CURRENT_DATE())
+ORDER BY Meses DESC LIMIT 10;
+
