0dc5709a6d9a5624eefce201d84762e74735bf19
Marcelo Silveira <silveira.ti@gmail.com>
2021-12-02 07:58:52 -0300

    Ajuste de nome de variáveis
    Meses->no_dias

--- Etapa_2.sql
+++ Etapa_2.sql
@@ -1,14 +1,18 @@
-﻿SELECT 
+﻿
+# Feito por Marcelo da Silva Silveira
+# 02/12/2021
+
+SELECT DISTINCT
    employees.first_name, #Primeiro nome do Funcionário
    employees.last_name,  #Último nome do Funcionário
    departments.dept_name, #Nome do Departamento
-   DATEDIFF(to_date, from_date) AS Meses #Converte o range de datas em dias
+   DATEDIFF(to_date, from_date) AS no_dias #Converte o range de datas em dias
 FROM  dept_emp #Tabela 
       INNER JOIN departments                      #\
         ON dept_emp.dept_no = departments.dept_no # |
                                                   # > Junção das tabelas necessárias para formar a tabela departamentos
       INNER JOIN employees                        # |
         ON dept_emp.emp_no = employees.emp_no     #/  
-WHERE year(to_date) < year(CURRENT_DATE()) AND  year(from_date) < year(CURRENT_DATE()) #Dispositivo de proteção para datas com erros
-ORDER BY Meses DESC LIMIT 10; # Ordena de forma descendente e limita a 10 itens
+WHERE to_date < CURRENT_DATE() AND  from_date < CURRENT_DATE() #Dispositivo de proteção para datas com erros
+ORDER BY no_dias DESC; # Ordena de forma descendente e limita a 10 itens
 
